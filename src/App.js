f753c650571b7d0aebc372c1f9680e736a1a6a99
Hamna Khan <hamnahameed24@gmail.com>
2023-09-18 20:34:14 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App=()=>{
+
+  const[items,setItems]=useState(" ")
+  const[itemsArray,setItemsArray]=useState([])
+
+  const textValue=(e)=>{
+    console.log(e.target.value)
+    setItems(e.target.value)
+  }
+
+  const addItem=()=>{
+    setItemsArray((allItems)=>{
+      return[...allItems,items]
+    }
+    );
+    setItems('')
+  }
+
+  const delFunc=(index)=>{
+      console.log("deleted")
+    
+      setItemsArray((allItems)=>{
+        return allItems.filter((arrayElem,i)=>{
+         return i!==index;
+        })
+      })
+  }
+
+
+  return(
+    <>
+    <div className="main_div">
+    <div className="center_div">
+        <h1>ToDo App</h1>
+        <input type="text" placeholder="Add a item"  onChange={textValue} value={items}/>
+        <button className="addBtn" onClick={addItem}>+</button>
+        
+
+        <ul>
+          {itemsArray.map((arrayItems,index)=>{
+            return (
+              <>
+              <div className="list_style" key={index} >
+              <button className="delBtn" onClick={() => delFunc(index)}>X</button>
+              <li> {arrayItems}</li>
+              </div>
+           
+              </>)
+          })}
+        </ul>
+      </div>
     </div>
-  );
+      
+    </>
+  )
 }
 
 export default App;
